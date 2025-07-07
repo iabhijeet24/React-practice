331eba1ce91830a89324b6f71be8842799c5dadf
Abhijeet Singh <inabhijeet241107@gmail.com>
2025-07-07 21:50:29 +0530

    State Hook in React

---
+++ CounterReact/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+
+
+function App() {
+
+  return (
+    <>
+      <counter/>
+    </>
+  )
+}
+
+export default App
---
+++ CounterReact/src/components/counter.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import "../components/counter.css"
+
+
+const counter = () => {
+  return (
+    <div className='counter-card'>Doggie</div>
+  )
+}
+
+export default counter
