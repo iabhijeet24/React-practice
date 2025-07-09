531a26e98c2d032dd3118dc668fe187600f04f90
Abhijeet Singh <inabhijeet241107@gmail.com>
2025-07-10 00:58:50 +0530

    Sending props in React

--- IntroReact/src/App.jsx
+++ IntroReact/src/App.jsx
@@ -1,13 +1,28 @@
 import UserCard from "../src/components/usercard";
+import "./App.css";
 
 function App() {
   return (
     <div className="wrapper">
-      <UserCard />
-      <UserCard />
-      <UserCard />
+ <UserCard
+        name="ABHIJEET SINGH"
+        image="https://robohash.org/abhijeet"
+        age="18"
+      />
+
+      <UserCard
+        name="ARNAV BANERJEE"
+        image="https://robohash.org/abhijeetArnav"
+        age="21"
+      />
+
+      <UserCard
+        name="PRIYA SHARMA"
+        image="https://robohash.org/PriyaSharma"
+        age="32"
+      />
     </div>
-  );
+  );  
 }
 
 export default App;
--- IntroReact/src/components/usercard.jsx
+++ IntroReact/src/components/usercard.jsx
@@ -2,13 +2,14 @@
 import React from "react";
 import "../components/usercard.css";
 
-// ✅ Then your component
-const UserCard = () => {
+const UserCard = ({name , age , image}) => {
+ 
   return (
     <div className="user-container">
-      <h3 id="user-name">Abhijeet Singh</h3>
-      <img src="" id="user-image" alt="user" />
-      <p id="user-desc">Description of ABHIJEET SINGH</p>
+       <img src={image} id="user-image" alt={`${name}'s profile picture`} />
+      <h3 id="user-name">{name}</h3>
+      <h4 id="user-age">{age}</h4>
+      <p id="user-desc">Description of {name}</p>
     </div>
   );
 };
